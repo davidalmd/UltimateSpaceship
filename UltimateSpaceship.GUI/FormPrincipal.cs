dc6636d2515985437e0b64672492e35498791fdb
David Almeida <david.almeida@grupomoura.com>
2024-01-12 16:57:33 -0300

    Inicio do desenvolvimento da GUI do jogo (criação do método de iniciar jogo)

--- UltimateSpaceship.GUI/FormPrincipal.cs
+++ UltimateSpaceship.GUI/FormPrincipal.cs
@@ -1,20 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using UltimateSpaceship.GameLogic;
+using System.Windows.Threading;
+using System.Drawing;
 
 namespace UltimateSpaceship.GUI
 {
     public partial class FormPrincipal : Form
     {
+        DispatcherTimer gameLoopTimer { get; set; }
+        Bitmap screenBuffer { get; set; }
+        Graphics screenPainter { get; set; }
+        Background background { get; set; }
         public FormPrincipal()
         {
             InitializeComponent();
+            this.screenBuffer = new Bitmap(Media.fundo.Width, Media.fundo.Height);
+            this.screenPainter = Graphics.FromImage(this.screenBuffer);
+            this.background = new Background(this.screenBuffer.Size, this.screenPainter);
+            this.gameLoopTimer = new DispatcherTimer(DispatcherPriority.Render);
+
         }
 
         private void FormPrincipal_Load(object sender, EventArgs e)
