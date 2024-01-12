04fddce85307162e39a89b8b2a0315b936b7580a
David Almeida <david.almeida@grupomoura.com>
2024-01-12 16:05:35 -0300

    Class GameObject created

--- UltimateSpaceship.GameLogic/GameObject.cs
+++ UltimateSpaceship.GameLogic/GameObject.cs
@@ -4,7 +4,7 @@ using System.Media;
 
 namespace UltimateSpaceship.GameLogic
 {
-    public class GameObject
+    public abstract class GameObject
     {
         #region Game Object Properties
         public bool Active { get; set; }
@@ -22,6 +22,82 @@ namespace UltimateSpaceship.GameLogic
         private SoundPlayer SoundPlayer { get; set; }
         #endregion
 
+        #region Game Object Methods
 
+        public abstract Bitmap GetSprite();
+
+        public GameObject(Size bounds, Graphics screen)
+        {
+            this.Bounds = bounds;
+            this.Screen = screen;
+            this.Active = true;
+            this.SoundPlayer = new SoundPlayer();
+            this.Sprite = this.GetSprite();
+            this.Rectangle = new Rectangle(this.Left, this.Top, this.Width, this.Height);
+        }
+
+        public virtual void UpdateObject()
+        {
+            this.Rectangle = new Rectangle(this.Left, this.Top, this.Width, this.Height);
+            this.Screen.DrawImage(this.Sprite, this.Rectangle);
+        }
+
+        public virtual void MoveLeft()
+        {
+            if (this.Left > 0)
+            {
+                this.Left -= this.Speed;
+            }
+        }
+
+        public virtual void MoveRight()
+        {
+            if (this.Left < this.Bounds.Width - this.Width)
+            {
+                this.Left += this.Speed;
+            }
+        }
+
+        public virtual void MoveDown()
+        {
+            this.Top += this.Speed;
+        }
+
+        public virtual void MoveUp()
+        {
+            this.Top -= this.Speed;
+        }
+
+        public bool isOutOfBounds()
+        {
+            return
+                (this.Top > this.Bounds.Height + this.Height) ||
+                (this.Top < -this.Height) ||
+                (this.Left > this.Bounds.Width + this.Width) ||
+                (this.Left < -this.Width);
+        }
+
+        public void PlaySound()
+        {
+            this.SoundPlayer.Stream = this.Sound;
+            this.SoundPlayer.Play();
+        }
+
+        public bool isCollidingWith(GameObject gameObject)
+        {
+            if (this.Rectangle.IntersectsWith(gameObject.Rectangle))
+            {
+                this.PlaySound();
+                return true;
+            }
+            return false;
+        }
+
+        public void Destroy()
+        {
+            this.Active = false;
+        }
+
+        #endregion
     }
 }
